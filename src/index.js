e0ee554ab3b8568e1851109beb36b08fe4bdd2ea
Toan <tpkhuong@gmail.com>
2020-12-01 15:24:34 -0500

    add codes to src index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 var express = require("express");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", function rootPage(req, res) {
-  res.send("../gettingBetter.html");
+  res.send("Hello");
 });
 
-app.listen(3000, function ourServer() {
+app.listen(PORT, function ourServer() {
   console.log(`App listen on port 3000`);
 });
