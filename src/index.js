ffb915b01c3277e0014e3a9d963f0de4f71829ec
Toan <tpkhuong@gmail.com>
2020-12-02 16:31:47 -0500

    continue working on header of navbar-scroll

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 var express = require("express");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.get("/", function rootPage(req, res) {
-  res.send("../gettingBetter.html");
+  res.render("gettingBetter");
 });
 
-app.listen(3000, function ourServer() {
-  console.log(`App listen on port 3000`);
+app.listen(PORT, function ourServer() {
+  console.log(`App listen on port ${PORT}`);
 });
