bb27f8b98085c47e666d9baf9380fe4c992e055e
Toan <tpkhuong@gmail.com>
2020-12-03 15:20:38 -0500

    our conditionals are working. next figure out how to add zeros to string

--- src/js/romanNumeralConverter.js
+++ src/js/romanNumeralConverter.js
@@ -29,6 +29,7 @@ function getDigit(num) {
   var lengthOfLoop = digitCount(num);
   var valueOfNum = [];
   var lengthOfEachValue = [];
+
   //looping lengthOfLoop
   /*arrayOfObjs will be [{value: 1000, length: 4}, {value: 900, length: 3}]*/
   var arrayOfObjs = [];
@@ -46,6 +47,19 @@ function getDigit(num) {
       length: digitCount(eachValue),
     });
   }
+
+  var valuesBeforeConvert = arrayOfObjs.map(function sameLengthArray({
+    value,
+    length,
+  }) {
+    return calculateNumsBeforeConvert(value, length);
+  });
+  /*** we will loop through array with our objs. we will pass in the value and length into our function ***/
+  /*** implement function that will return an array of subarrays: if we passded in 1985 we want an array that look like this:
+   * we want the array to hold string type of these values.
+   * [[1000], [100,1000],[50,10,10,10],[5]] then we can loop through this array and convert it to romanNumeral
+   * ***/
+  console.log(valuesBeforeConvert);
 }
 
 function digitCount(num) {
@@ -53,7 +67,46 @@ function digitCount(num) {
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
-function calculateOurNums(valueInput, lengthInput) {}
+function calculateNumsBeforeConvert(valueInput, lengthInput) {
+  var numsOfZerosToAddToEnd = lengthInput - 1;
+  var objOfZeros = {
+    1: "0",
+    2: "00",
+    3: "000",
+  };
+  console.log(
+    objOfZeros[numsOfZerosToAddToEnd],
+    typeof objOfZeros[numsOfZerosToAddToEnd]
+  );
+  var checkNumAndLoopThisNum = Number(valueInput[0]);
+  var arrOfNumsBeforeAddZeros = [];
+  /*** using triple equal just incase we can a string of 0, empty string "" or empty array [] ***/
+  if (checkNumAndLoopThisNum == 5) {
+    //.push() will return the length SO WE WERE PUSHING/RETURNING THE LENGTH TO OUR MAP METHOD. THAT IS WHY OUR ARRAY LOOKED LIKED THIS: [["1"],1,["5","1","1","1"],1]
+    arrOfNumsBeforeAddZeros.push("5");
+    return arrOfNumsBeforeAddZeros;
+  }
+  if (checkNumAndLoopThisNum == 4) {
+    arrOfNumsBeforeAddZeros.push("1", "5");
+    return arrOfNumsBeforeAddZeros;
+  }
+  if (checkNumAndLoopThisNum == 9) {
+    arrOfNumsBeforeAddZeros.push("1", "10");
+    return arrOfNumsBeforeAddZeros;
+  }
+  if (checkNumAndLoopThisNum < 5) {
+    for (let i = 0; i < checkNumAndLoopThisNum; i++) {
+      arrOfNumsBeforeAddZeros.push("1");
+    }
+    return arrOfNumsBeforeAddZeros;
+  } else {
+    arrOfNumsBeforeAddZeros.push("5");
+    for (let i = 6; i <= checkNumAndLoopThisNum; i++) {
+      arrOfNumsBeforeAddZeros.push("1");
+    }
+    return arrOfNumsBeforeAddZeros;
+  }
+}
 // [1000, 900, 80, 5].forEach(function findLength(eachValue) {
 //   console.log(digitCount(eachValue));
 // });
