e1a5d3d095fb0d3b5f331cd655874f6b818094bd
Toan <tpkhuong@gmail.com>
2021-01-09 09:51:28 -0500

    first solution for diagonalDifference complete

---
+++ public/js/funChallenges.js
@@ -0,0 +1,61 @@
+/* diagonal sum */
+/*
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+[[3],[1, 2, 3],
+[4, 5, 6],
+[9, 8, 9]]  
+
+[[3],
+[11, 2, 4],
+[4, 5, 6],
+[10, 8, -12]]
+
+Function description
+
+Complete the  function in the editor below.
+
+diagonalDifference takes the following parameter:
+
+int arr[n][m]: an array of integers
+Return
+
+int: the absolute diagonal difference
+Input Format
+
+*/
+
+function diagonalDifference(arrInput) {
+  var arrayMatrix = checkLength(arrInput);
+  var [firstSubarray, secondSubarray, thirdSubarray] = arrayMatrix;
+  var [leftFirstValue, ignore, rightFirstValue] = firstSubarray;
+  var [ignoreTwo, useInBothCalc, ignoreThree] = secondSubarray;
+  var [rightSecondValue, ignoreFour, leftSecondValue] = thirdSubarray;
+
+  var leftSideCalc = calcTheSum([
+    leftFirstValue,
+    useInBothCalc,
+    leftSecondValue,
+  ]);
+  var rightSideCalc = calcTheSum([
+    rightFirstValue,
+    useInBothCalc,
+    rightSecondValue,
+  ]);
+
+  return Math.abs(leftSideCalc - rightSideCalc);
+}
+
+function calcTheSum(arrInput) {
+  return arrInput.reduce(function addStuff(buildingUp, currentValue) {
+    return (buildingUp += currentValue);
+  }, 0);
+}
+
+function checkLength(arrInput) {
+  return arrInput.filter(function sameLengthArrOnly(eachValue) {
+    return eachValue.length == 3;
+  });
+}
